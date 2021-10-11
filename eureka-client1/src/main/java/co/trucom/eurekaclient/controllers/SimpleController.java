528b55fa4e279a10d0b38c561ff9bea295a18a87
gromao <guilherme.romao@sebasi.pt>
2021-10-11 12:53:04 +0100

    Adding files

---
+++ eureka-client1/src/main/java/co/trucom/eurekaclient/controllers/SimpleController.java
@@ -0,0 +1,66 @@
+package co.trucom.eurekaclient.controllers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class SimpleController {
+	
+	@Autowired
+	private LoadBalancerClient loadBalancer;
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@GetMapping("hello")
+	public ResponseEntity<String> sayHello() {
+		return new ResponseEntity<>("Hi", HttpStatus.OK);
+	}
+	
+	@GetMapping("callother")
+	public ResponseEntity<String> callOtherClient() {
+		try {
+			String content = restTemplate.getForObject(getBaseUrl() + "/api/v1/calling-another-client", String.class);
+			
+			return new ResponseEntity<>(content, HttpStatus.OK);
+		} catch(Exception e) {
+			throw new RuntimeException("Couldn't get String from the other client. Error: " 
+					+ e);
+		}
+	}
+	
+	@GetMapping("coldplay") 
+	public ResponseEntity<Map<String, String>> coldplayLyrics() {
+		String apiUrl = "https://api.lyrics.ovh/v1/Coldplay/Adventure of a Lifetime";
+		try {
+			String content = restTemplate.getForObject(apiUrl, String.class);
+			Map<String, String> responseMap = new HashMap<>();
+			responseMap.put("lyrics", content);
+			
+			return new ResponseEntity<>(responseMap, HttpStatus.OK);
+		} catch(Exception e) {
+			throw new RuntimeException("Couldn't get lyrics from API. Error: " 
+					+ e);
+		}
+	}
+	
+	private String getBaseUrl() {
+		ServiceInstance serviceInstance = loadBalancer.choose("ANOTHER-CLIENT");
+		
+		System.out.println(serviceInstance.getUri().toString());
+		
+		return serviceInstance.getUri().toString();
+	}
+
+}
---
+++ eureka-client1/src/test/java/co/trucom/eurekaclient/EurekaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package co.trucom.eurekaclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
